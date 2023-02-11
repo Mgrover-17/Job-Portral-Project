46136c205b7688ddba164c78edab4e72f166f845
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-02-12 01:47:03 +0530

    database query added to login

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Coin-Int-Project
 Project on Job-Portral
 09/2/23 -->Register and Login Page
+12/2/23 -->Database Connection and taken data from users and register them in the database and login
 
--- login.php
+++ login.php
@@ -1,3 +1,55 @@
+<?php
+//This script will handle login
+session_start();
+
+// check if the user is already logged in
+if(isset($_SESSION['username']))
+{
+    header("location: index.php");
+    exit;
+}
+require_once "config.php";
+
+$username = $password = "";
+$err = "";
+
+// if request method is post
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(empty(trim($_POST['username'])) || empty(trim($_POST['password'])))
+    {
+        $err = "Please enter username + password";
+    }
+    else{
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+    }
+if(empty($err))
+{
+    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $param_username);
+    $param_username = $username;
+    
+    
+    // Try to execute this statement
+    if(mysqli_stmt_execute($stmt)){
+        mysqli_stmt_store_result($stmt);
+        if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt))
+                    {
+                        if(password_verify($password, $hashed_password))
+                        {
+                            // this means the password is corrct. Allow user to login
+                            session_start();
+                            $_SESSION["username"] = $username;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["loggedin"] = true;
+
+                            //Redirect user to welcome page
+                            header("location: index.php");
+                            }}}}}}?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -170,13 +222,13 @@
         <form  class="login-form" method="POST">
             <div class="textbox">
                 <input type="email" placeholder="Username" name="username" />
-                <span><i class="fa-solid fa-user"></i>
+                <span><i class="fas fa-user user input-icons"></i>
                 </span>
             </div>
             <div class="textbox">
                 <input type="password" placeholder="Password" name="password"/>
                 <span>
-                <i class="fa-regular fa-lock"></i>
+                <i class="fas fa-key key input-icons"></i>
                 </span>
             </div>
             <button type="submit" name="Login">LOGIN</button>
